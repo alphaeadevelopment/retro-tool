8f2046f100617c64ba315a94fdb94e48d63c15fd
Graham King <gking2224@gmail.com>
2018-03-15 23:24:37 +0000

    add response type bug

--- src/server/socket/handlers/add-response-type.js
+++ src/server/socket/handlers/add-response-type.js
@@ -5,9 +5,9 @@ export default (io, socket) => (data) => { // eslint-disable-line no-unused-vars
   const onError = emitError(socket);
   return connectionManager.getConnection(socket.id)
     .then((connection) => {
+      const { sessionId } = connection;
       sessionManager.addResponseType(connection, data)
         .then((newResponseType) => {
-          const sessionId = sessionManager.getSessionId(socket.id);
           io.to(sessionId).emit('responseTypeAdded', { responseType: newResponseType });
         })
         .catch(e => onError(e));
