ff524b7e3b36e0a1f0a3c1a6a7b3327336bbcb73
Graham King <gking2224@gmail.com>
2018-03-17 12:12:37 +0000

    footer with links

--- src/client/containers/App.jsx
+++ src/client/containers/App.jsx
@@ -6,11 +6,13 @@ import { MuiThemeProvider } from 'material-ui/styles';
 import theme from '../styles/theme';
 import { SessionLaunch } from '../pages';
 import Errors from './Errors';
+import Footer from './Footer';
 
 export const RawApp = () => (
   <MuiThemeProvider theme={theme}>
 
     <Route path={'/:sessionId?'} component={SessionLaunch} />
+    <Footer />
     <Errors />
   </MuiThemeProvider>
 );
