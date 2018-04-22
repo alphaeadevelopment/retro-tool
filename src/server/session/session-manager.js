927d764c2ce9cc7f78084f93084788fb728f243b
Graham King <gking2224@gmail.com>
2018-04-22 14:30:21 +0100

    delete old commented-out code

--- src/server/session/session-manager.js
+++ src/server/session/session-manager.js
@@ -16,8 +16,6 @@ class SessionManager {
       .catch(e => rej(e));
   });
   createSession = (sessionId, socket, owner) => new Promise((res, rej) => {
-    // connectionManager.registerSocket(socket.id, owner, sessionId, token)
-    //   .then(() => {
     const session = newSession(sessionId, owner);
     dao.save(session)
       .then((saved) => {
@@ -25,8 +23,6 @@ class SessionManager {
         res(saved);
       })
       .catch(e => rej(e));
-    // })
-    // .catch(e => rej(e));
   })
   getOwnerSocket = sessionId => new Promise((res, rej) => {
     this.getOwner(sessionId)
@@ -48,18 +44,12 @@ class SessionManager {
       .catch(e => rej(e));
   })
   joinSession = (socket, name, sessionId) => new Promise((res, rej) => {
-    // connectionManager.socketRegistered(socket.id)
-    //   .then((registered) => {
-    //     if (registered) rej(new Error('already in session'));
-    //     else {
     this.getSession(sessionId)
       .then((session) => {
         if (has(session.participants, name)) {
           rej(new Error('name in use'));
         }
         else {
-          // connectionManager.registerSocket(socket.id, name, sessionId, token)
-          //   .then(() => {
           const newParticipant = {
             id: name,
             name,
@@ -72,65 +62,39 @@ class SessionManager {
               res(updated);
             })
             .catch(e => rej(e));
-          // })
-          // .catch(e => rej(e));
         }
       })
       .catch(e => rej(e));
-    //   }
-    // })
-    // .catch(e => rej(e));
   });
   getSession = sessionId => dao.getSession(sessionId)
   leaveSession = ({ name, sessionId }) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ name, sessionId }) => {
     dao.removeParticipant(sessionId, name)
       .then(() => {
         socketManager.removeSocket(name);
         res(sessionId);
       })
       .catch(e => rej(e));
-    // })
-    // .catch(e => rej(e));
   });
   disconnect = ({ name, sessionId }) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ name, sessionId }) => {
     socketManager.removeSocket(name);
-    // connectionManager.deregisterSocket(socketId)
-    //   .then(() => {
     if (name) {
       dao.participantDisconnected(sessionId, name)
         .then(() => res())
         .catch(e => rej(e));
     }
     else res();
-    // })
-    // .catch(e => rej(e));
-    // })
-    // .catch(e => rej(e));
   });
   reconnect = ({ sessionId, name }, socket) => new Promise((res, rej) => {
-    // connectionManager.getConnectionByToken(token)
-    //   .then(({ sessionId, name }) => {
     if (!name && !sessionId) rej(new Error('unknown token'));
-    // connectionManager.registerSocket(socket.id, name, sessionId, token)
-    //   .then(() => {
     dao.participantReconnected(sessionId, name)
       .then((updatedSession) => {
         socketManager.saveSocket(name, socket);
         res(updatedSession);
       })
       .catch(e => rej(e));
-    //     })
-    //     .catch(e => rej(e));
-    // });
   })
   addResponseType = ({ sessionId }, data) => new Promise((res, rej) => {
     const id = generate();
-    // connectionManager.getConnection(socketId)
-    //   .then(({ sessionId }) => {
     const { question, ...rest } = data;
     const newResponseType = { id, title: question, ...rest };
     dao.addResponseType(sessionId, newResponseType)
@@ -138,8 +102,6 @@ class SessionManager {
       .catch(e => rej(e));
   })
   addResponse = ({ name, sessionId }, responseType, value) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ name, sessionId }) => {
     const id = generate();
     const newResponse = {
       id, author: name, response: value, responseType, votes: [],
@@ -147,70 +109,45 @@ class SessionManager {
     dao.addResponse(sessionId, newResponse)
       .then(updatedSession => res(updatedSession.responses[id]))
       .catch(e => rej(e));
-    // });
   });
   upVoteResponse = ({ sessionId, name }, responseId) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ name, sessionId }) => {
     dao.upVoteResponse(sessionId, responseId, name)
       .then(() => res())
       .catch(e => rej(e));
-    // });
   });
   cancelUpVoteResponse = ({ name, sessionId }, responseId) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ name, sessionId }) => {
     dao.cancelUpVoteResponse(sessionId, responseId, name)
       .then(() => res())
       .catch(e => rej(e));
-    // });
   });
   deleteResponse = ({ sessionId }, responseId) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ name, sessionId }) => {
     dao.deleteResponse(sessionId, responseId)
       .then(() => res())
       .catch(e => rej(e));
-    // });
   });
   deleteResponseType = ({ sessionId }, responseTypeId) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ name, sessionId }) => {
     dao.deleteResponseType(sessionId, responseTypeId)
       .then(() => res())
       .catch(e => rej(e));
-    // });
   });
   setStatus = ({ sessionId }, status) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ sessionId }) => {
     dao.setStatus(sessionId, status)
       .then(updatedSession => res(updatedSession))
       .catch(e => rej(e));
-    // })
-    // .catch(e => rej(e));
   })
   addFeedback = ({ sessionId }, responseId, message) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ sessionId }) => {
     dao.addFeedback(sessionId, responseId, message)
       .then((updated) => {
         res(updated.responses[responseId]);
       })
       .catch(e => rej(e));
-    // })
-    // .catch(e => rej(e));
   })
   savePdfData = ({ sessionId, data }) => new Promise((res, rej) => {
-    // connectionManager.getConnection(socketId)
-    //   .then(({ sessionId }) => {
     dao.savePdfData(sessionId, data)
       .then(() => {
         res();
       })
       .catch(e => rej(e));
-    // })
-    // .catch(e => rej(e));
   })
 }
 
